fc10eb9e69f28d4df7cc912be84dfadfda88ab73
ankit <95940970+ankitsamaddar@users.noreply.github.com>
2024-12-17 13:42:45 +0530

    fix🛠️: update changes to the structure🐛

--- wp-config.php
+++ wp-config.php
@@ -27,27 +27,42 @@ define('WP_SITEURL', 'http://' . $_SERVER['HTTP_HOST'] . '/wordpress');
 /**
  * WordPress Content and Plugin Directories
  */
-define( 'UPLOADS', '/wp-uploads' ); // Set Uploads folder
+define( 'UPLOADS', 'content/wp-uploads' ); // Set Uploads folder
 
-define('WP_CONTENT_DIR', dirname(__FILE__) . '/wp-content');
-define( 'WP_CONTENT_URL', 'http://' . $_SERVER['HTTP_HOST'] . '/wp-content' );
-define('WP_PLUGIN_DIR', dirname(__FILE__) . '/wp-content/plugins');
-define('WP_PLUGIN_URL', 'http://' . $_SERVER['HTTP_HOST'] . '/wp-content/plugins');
+define('WP_CONTENT_DIR', dirname(__FILE__) . '/content/wp-content');
+define( 'WP_CONTENT_URL', 'http://' . $_SERVER['HTTP_HOST'] . '/content/wp-content' );
+define('WP_PLUGIN_DIR', dirname(__FILE__) . '/content/wp-content/plugins');
+define('WP_PLUGIN_URL', 'http://' . $_SERVER['HTTP_HOST'] . '/content/wp-content/plugins');
+
+// Autolod the plugins
+require __DIR__ . '/content/vendor/autoload.php';
+
+
+// Define environment variables
+if (file_exists(__DIR__ . '/.env')) {
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__, '/.env');
+    $dotenv->load();
+	echo 'Environment variables loaded:<br>';
+    echo 'DB_NAME = ' . $_ENV['DB_NAME'] . '<br>';
+    echo 'DB_USER = ' . $_ENV['DB_USER'] . '<br>';
+    echo 'DB_PASSWORD = ' . $_ENV['DB_PASSWORD'] . '<br>';
+    echo 'DB_HOST = ' . $_ENV['DB_HOST'] . '<br>';
+}
+
 
 /**
  * Debugging Settings
  * WARNING: CHANGE THIS TO FALSE IN PRODUCTION
  * @link https://wordpress.org/support/article/debugging-in-wordpress/
  */
- */
-define( 'WP_DEBUG', getenv('WP_DEBUG') === 'true');
+define( 'WP_DEBUG', $_ENV['WP_DEBUG'] === 'true');
 if ( WP_DEBUG ) {
     define( 'WP_DEBUG_LOG', true ); // Errors logged in wp-content/debug.log
-    define( 'WP_DEBUG_DISPLAY', ture );
+    define( 'WP_DEBUG_DISPLAY', true );
     @ini_set( 'display_errors', 1 );
 }
 // Use dev versions of core JS and CSS files (only needed if you are modifying these core files)
-define( 'SCRIPT_DEBUG', getenv('SCRIPT_DEBUG') === 'true' );
+define( 'SCRIPT_DEBUG', $_ENV['SCRIPT_DEBUG'] === 'true' );
 
 /**
  * File Modifications and Updates
@@ -55,50 +70,45 @@ define( 'SCRIPT_DEBUG', getenv('SCRIPT_DEBUG') === 'true' );
 define('DISALLOW_FILE_MODS', false); // Enable in development
 define('WP_AUTO_UPDATE_CORE', false); // Managed via composer
 
-
-// Autolod the plugins
-require __DIR__ . '/wp-content/vendor/autoload.php';
-
-// Define environment variables
-if (file_exists(__DIR__ . '/.env')) {
-    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-    $dotenv->load();
-}
-
 /**
  * MySQL Database settings
 */
-define('DB_NAME', getenv('DB_NAME') ?: 'wordpress');
-define('DB_USER', getenv('DB_USER') ?: 'root');
-define('DB_PASSWORD', getenv('DB_PASSWORD') ?: 'root');
-define('DB_HOST', getenv('DB_HOST') ?: 'localhost');
+define('DB_NAME', $_ENV['DB_NAME']);
+define('DB_USER', $_ENV['DB_USER']);
+define('DB_PASSWORD', $_ENV['DB_PASSWORD']);
+define('DB_HOST', $_ENV['DB_HOST']);
+
 /* WordPress DB Charset (is setup this way when the tables are made)  */
 define('DB_CHARSET', 'utf8mb4');
+
 // WordPress DB Collation (is setup this way when the tables are made)
 define( 'DB_COLLATE', 'utf8_general_ci' );
 
 // Database table prefix
 $table_prefix = 'wpdb_';
 
+
 /** Authentication unique keys and salts
  * @link https://api.wordpress.org/secret-key/1.1/salt/
 */
-define('AUTH_KEY',         getenv('AUTH_KEY'));
-define('SECURE_AUTH_KEY',  getenv('SECURE_AUTH_KEY'));
-define('LOGGED_IN_KEY',    getenv('LOGGED_IN_KEY'));
-define('NONCE_KEY',        getenv('NONCE_KEY'));
-define('AUTH_SALT',        getenv('AUTH_SALT'));
-define('SECURE_AUTH_SALT', getenv('SECURE_AUTH_SALT'));
-define('LOGGED_IN_SALT',   getenv('LOGGED_IN_SALT'));
-define('NONCE_SALT',       getenv('NONCE_SALT'));
+define('AUTH_KEY',         $_ENV['AUTH_KEY']);
+define('SECURE_AUTH_KEY',  $_ENV['SECURE_AUTH_KEY']);
+define('LOGGED_IN_KEY',    $_ENV['LOGGED_IN_KEY']);
+define('NONCE_KEY',        $_ENV['NONCE_KEY']);
+define('AUTH_SALT',        $_ENV['AUTH_SALT']);
+define('SECURE_AUTH_SALT', $_ENV['SECURE_AUTH_SALT']);
+define('LOGGED_IN_SALT',   $_ENV['LOGGED_IN_SALT']);
+define('NONCE_SALT',       $_ENV['NONCE_SALT']);
 
 // Define the environment
 define('WP_ENVIRONMENT_TYPE', 'local');
-/* That's all, stop editing! Happy blogging. */
+/**
+ * That's all, stop editing! Happy blogging.
+**/
 
 /* Absolute path to the WordPress directory. */
 if (!defined('ABSPATH')) {
-  define('ABSPATH',  dirname(__FILE__) . '/public/');
+  define('ABSPATH',   __DIR__ . '/wordpress');
 }
 
 /* Sets up WordPress vars and included files. */
